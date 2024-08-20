bae0b9048c9f172d97bb43f184bb086689d7615f
RISHI <140811826+RISHI-1453@users.noreply.github.com>
2024-08-20 15:23:51 +0530

    Update CustomerDTO.java

--- Week 4/Exercise 8/CustomerDTO.java
+++ Week 4/Exercise 8/CustomerDTO.java
@@ -1,11 +1,27 @@
-package com.bookstoreapi.dto;
+package com.bookstoreapi.entity;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class CustomerDTO {
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Size
+    @Size(min = 1, max = 100)
+    private String name;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @Version
+    private Long version;
+
+    // Getters and Setters
+}
